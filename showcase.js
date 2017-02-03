80a69d286ee03b7007eea27aea28de704914d48a
Alfredo Hinojosa <alfhh4@gmail.com>
2017-02-02 22:43:43 -0600

    Basic functionallity

--- showcase.js
+++ showcase.js
@@ -1,35 +1,78 @@
-function init() {
+/**
+ * ShowcaseJS developed by Alfredo Hinojosa
+ * Version 0.1 Feb/2017
+ * MIT License
+ */
 
-    var gitServices = {};
-    var repos = [];
-    var gitUser = {};
 
-    window.gitServices = gitServices;
-    window.repos = repos;
-    window.gitUser = gitUser;
+showcase = {}; // Object where the user repositories are stored
+showcaseUtilities = {}; // Utilities that ShowcaseJS uses
 
-    gitUser.userName = "alfhh";
+/**
+ * Setup the required variables for ShowcaseJS to work, it also generates the specific
+ * service URLs for the GitHub user.
+ * @param userName of the GitHub user
+ */
+function setupShowcase(userName) {
 
-    gitServices.getUserServiceURL = "https://api.github.com/users/" + gitUser.userName;
-    gitServices.getUserReposServiceURL = "https://api.github.com/users/" + gitUser.userName + "/starred";
+    // Creation of variables
+    showcaseUtilities.gitServices = {};
+    showcase.gitUser = {};
+    showcase.repos = [];
 
-    // Get the id of the user
-    $.get(gitServices.getUserServiceURL, function (data) {
+    // Save the userName
+    showcase.gitUser.userName = userName;
 
-        gitUser.id = data.id;
-        getUserRepos();
-
-    });
+    // Generate the service URL depending of the userName
+    showcaseUtilities.gitServices.getUserServiceURL = "https://api.github.com/users/" + showcase.gitUser.userName;
+    showcaseUtilities.gitServices.getUserReposServiceURL = "https://api.github.com/users/"
+        + showcase.gitUser.userName + "/starred";
 
 }
 
-function getUserRepos() {
+/**
+ * Call the specified GitHub service
+ * @param serviceURL
+ * @returns {*} returns a promise
+ */
+function callGithubService(serviceURL) {
 
-    // Get the id of the user
-    $.get(gitServices.getUserReposServiceURL, function (data) {
+    var deferred = $.Deferred(); // Create a promise
+    var promise = $.get(serviceURL); // Make the API Call
 
-        console.log(data);
-
+    $.when(promise).done(function (result) {
+        deferred.resolve(result); // Return the JSON response
     });
+
+    return deferred.promise();
+
+}
+
+function showcaseInit(userName) {
+    setupShowcase(userName);
+
+    // Get the GitHub ID of the user
+    callGithubService(showcaseUtilities.gitServices.getUserServiceURL)
+        .then(function (userJSON) {
+
+            // Save the userId
+            showcase.gitUser.userId = userJSON.id;
+
+            // Get the starred repos by user
+            callGithubService(showcaseUtilities.gitServices.getUserReposServiceURL)
+                .then(function (reposJSON) {
+
+                    $.each(reposJSON, function (index, obj) {
+
+                        // Check if the owner of the repo is the GitHub user
+                        if (obj.owner.id === showcase.gitUser.userId) {
+                            showcase.repos.push(obj);
+                        }
+                    });
+
+                    console.log(showcase.repos);
+
+                })
+        })
 
 }
